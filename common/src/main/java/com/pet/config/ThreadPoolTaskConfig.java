897d929786399405e806d2037fe1f4b404610d7e
武岳 <wuyue930912@live.com>
2021-01-28 13:53:42 +0800

    1、定时任务例子 🙃

--- common/src/main/java/com/pet/config/ThreadPoolTaskConfig.java
+++ common/src/main/java/com/pet/config/ThreadPoolTaskConfig.java
@@ -13,23 +13,31 @@ import java.util.concurrent.ThreadPoolExecutor;
 @Configuration
 @EnableAsync
 public class ThreadPoolTaskConfig {
-    private static final int CORE_POOL_SIZE = Runtime.getRuntime().availableProcessors() *2;
+    private static final int CORE_POOL_SIZE = Runtime.getRuntime().availableProcessors() * 2;
     private static final int MAX_POOL_SIZE = Math.max(CORE_POOL_SIZE * 4, 256);
     private static final int KEEP_ALIVE_TIME = 10; //允许线程空闲时间（单位为秒）
     private static final int QUEUE_CAPACITY = 200; // 缓冲队列数
     private static final int AWAIT_TERMINATION = 60;//线程池中任务的等待时间，如果超过这个时候还没有销毁就强制销毁
     private static final Boolean WAIT_FOR_TASKS_TO_COMPLETE_ON_SHUTDOWN = true;//用来设置线程池关闭的时候等待所有任务都完成再继续销毁其他的Bean
-    private static final String THREAD_NAME_PREFIX = "Pet-Service-"; // 线程池名前缀
 
 
     @Bean("logTaskExecutor")
-    public ThreadPoolTaskExecutor piceaTaskExecutor () {
+    public ThreadPoolTaskExecutor logTaskExecutor() {
+        return getThreadPoolTaskExecutor("log-thread-");
+    }
+
+    @Bean("schedulingTaskExecutor")
+    public ThreadPoolTaskExecutor schedulingTaskExecutor() {
+        return getThreadPoolTaskExecutor("scheduling-thread");
+    }
+
+    private ThreadPoolTaskExecutor getThreadPoolTaskExecutor(String s) {
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
         taskExecutor.setCorePoolSize(CORE_POOL_SIZE);
         taskExecutor.setMaxPoolSize(MAX_POOL_SIZE);
         taskExecutor.setKeepAliveSeconds(KEEP_ALIVE_TIME);
         taskExecutor.setQueueCapacity(QUEUE_CAPACITY);
-        taskExecutor.setThreadNamePrefix(THREAD_NAME_PREFIX);
+        taskExecutor.setThreadNamePrefix(s);
         taskExecutor.setWaitForTasksToCompleteOnShutdown(WAIT_FOR_TASKS_TO_COMPLETE_ON_SHUTDOWN);
         taskExecutor.setAwaitTerminationSeconds(AWAIT_TERMINATION);
         taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
---
+++ common/src/main/java/com/pet/constant/CronConstant.java
@@ -0,0 +1,5 @@
+package com.pet.constant;
+
+public class CronConstant {
+    public static final String ONE_HOUR = "0 0 0/1 * * ? ";
+}
