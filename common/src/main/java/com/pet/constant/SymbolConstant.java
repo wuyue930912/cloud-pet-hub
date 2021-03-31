e94e66fdf1a6c6acdc89f30e8a53235c510e07db
武岳 <wuyue930912@live.com>
2021-03-31 13:39:12 +0800

    1、Date工具类

---
+++ common/src/main/java/com/pet/constant/SymbolConstant.java
@@ -0,0 +1,11 @@
+package com.pet.constant;
+
+public class SymbolConstant {
+    private SymbolConstant() {
+    }
+
+    public static final String BAR = "-";
+    public static final String SEMICOLON = ":";
+    public static final String SPACE = " ";
+
+}
