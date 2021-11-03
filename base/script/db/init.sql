73ddef703a9d61e64d8d99e3c2a95ae1835a1901
武岳 <wuyue930912@live.com>
2021-11-03 16:39:34 +0800

    一个小例子

--- base/script/db/init.sql
+++ base/script/db/init.sql
@@ -4,78 +4,21 @@ use pethub;
 drop table sys_user;
 create table sys_user
 (
-    id varchar(64) null,
-    user_name varchar(64) null,
-    user_pwd varchar(128) null,
-    user_nick_name varchar(64) null,
-    user_icon varchar(128) null,
-    user_phone varchar(16) null,
-    user_email varchar(64) null,
-    create_time timestamp null,
-    create_user varchar(64) null,
-    modify_time timestamp null,
-    modify_user varchar(64) null,
-    delete_flag tinyint(1) null,
+    id             varchar(64)  null,
+    user_name      varchar(64)  null,
+    user_pwd       varchar(128) null,
+    user_nick_name varchar(64)  null,
+    user_icon      varchar(128) null,
+    user_phone     varchar(16)  null,
+    user_email     varchar(64)  null,
+    create_time    timestamp    null,
+    create_user    varchar(64)  null,
+    modify_time    timestamp    null,
+    modify_user    varchar(64)  null,
+    delete_flag    tinyint(1)   null,
     constraint sys_user_pk
         primary key (id)
-)engine=innoDB default charset=utf8;
+) engine = innoDB
+  default charset = utf8;
 
-create unique index sys_user_user_name_uindex
-    on sys_user (user_name);
-create table sys_user_role
-(
-    id varchar(64) null,
-    user_id varchar(64) null,
-    role_id varchar(64) null,
-    constraint sys_user_role_pk
-        primary key (id)
-)engine=innoDB default charset=utf8;
-
-create table sys_role
-(
-    id varchar(64) null,
-    role_name varchar(64) null,
-    role_describe varchar(64) null,
-    create_time timestamp null,
-    create_user varchar(64) null,
-    modify_time timestamp null,
-    modify_user varchar(64) null,
-    is_delete int null,
-    constraint sys_role_pk
-        primary key (id)
-)engine=innoDB default charset=utf8;
-
-create table sys_role_right
-(
-    id varchar(64) null,
-    role_id varchar(64) null,
-    right_id varchar(64) null,
-    constraint sys_role_right_pk
-        primary key (id)
-)engine=innoDB default charset=utf8;
-
-create table sys_right
-(
-    id varchar(64) null,
-    parent_id varchar(64) null,
-    right_name varchar(64) null,
-    right_icon varchar(64) null,
-    right_url varchar(64) null,
-    create_time timestamp null,
-    create_user varchar(64) null,
-    modify_time timestamp null,
-    modify_user varchar(64) null,
-    is_delete int null,
-    constraint sys_right_pk
-        primary key (id)
-)engine=innoDB default charset=utf8;
-
-create table sys_log
-(
-    id varchar(64) null,
-    log_describe varchar(64) null,
-    create_time timestamp null,
-    create_user varchar(64) null,
-    constraint sys_log_pk
-        primary key (id)
-)engine=innoDB default charset=utf8;
+create unique index sys_user_user_name_uindex on sys_user (user_name);
