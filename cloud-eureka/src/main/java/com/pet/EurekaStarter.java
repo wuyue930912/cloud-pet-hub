e3355bd473441e6e247195081573be655fb0ad78
武岳 <wuyue930912@live.com>
2021-11-30 13:20:27 +0800

    1、eureka 集成 security 认证

--- cloud-eureka/src/main/java/com/pet/EurekaStarter.java
+++ cloud-eureka/src/main/java/com/pet/EurekaStarter.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -14,4 +16,14 @@ public class EurekaStarter {
         SpringApplication.run(EurekaStarter.class, args);
         log.info("start running eureka server : 【{}】", "http://127.0.0.1:8888");
     }
+
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().ignoringAntMatchers("/eureka/**");
+            super.configure(http);
+        }
+    }
+
 }
